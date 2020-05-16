ba44faa81accf47f2a068767650270b03fd73932
Leyla Shakirzyanova <56456307+leylashakir@users.noreply.github.com>
2020-05-16 19:39:30 -0400

    Updated Chain.java file
    Added a method to verify that the new block's previous hash is the same as the last block's hash. If so, add the new block to the end of the blockchain.

--- src/main/java/htc/chain/Chain.java
+++ src/main/java/htc/chain/Chain.java
@@ -1,5 +1,32 @@
 package htc.chain;
 
+import java.util.ArrayList;
+
 public class Chain {
-  
+	
+	// Create array to append blocks
+	public ArrayList <Block> blockChain = new ArrayList <Block>();
+
+	public void addAndValidateChain(Block block) {
+		
+		Block current = block;
+		
+		// Check that the new block's previous hash is the same as the last block's
+		for (int i = blockChain.size() - 1; i >= 0; i--) {
+			Block b = blockChain.get(i);
+			
+			if (b.getHash().equals(current.getPreviousHash())) {
+				// if yes, current block will become the new block
+				current = b;
+			} 
+			
+			else {
+				throw new RuntimeException("Invalid");	
+			}
+		}
+		
+		// add the new block to the chain
+		this.blockChain.add(block);
+	}
+
 }
