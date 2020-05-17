371a45a5c295edc25b9972e2d44f3f8c3888d590
Leyla Shakirzyanova <56456307+leylashakir@users.noreply.github.com>
2020-05-16 21:27:12 -0400

    Changed the interface from ArrayList to Set
     Append blocks to chain without having to check if the block already exists in the chain, since there are no duplicate elements allowed in a Set.

--- src/main/java/htc/chain/Chain.java
+++ src/main/java/htc/chain/Chain.java
@@ -1,32 +1,13 @@
 package htc.chain;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Chain {
+	// Create hash set to append blocks
+	Set<Block> chain = new HashSet <Block>();
 	
-	// Create array to append blocks
-	public ArrayList <Block> blockChain = new ArrayList <Block>();
-
 	public void addAndValidateChain(Block block) {
-		
-		Block current = block;
-		
-		// Check that the new block's previous hash is the same as the last block's
-		for (int i = blockChain.size() - 1; i >= 0; i--) {
-			Block b = blockChain.get(i);
-			
-			if (b.getHash().equals(current.getPreviousHash())) {
-				// if yes, current block will become the new block
-				current = b;
-			} 
-			
-			else {
-				throw new RuntimeException("Invalid");	
-			}
-		}
-		
-		// add the new block to the chain
-		this.blockChain.add(block);
+		chain.add(block);
 	}
-
 }
