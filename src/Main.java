bceb966eb8f40ea689f8f6acac33b3be7afe1e14
Leyla Shakirzyanova <56456307+leylashakir@users.noreply.github.com>
2020-05-17 02:55:32 -0400

    Uploaded Main file for app window and .fxml for layout

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("CryptoWallet");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
