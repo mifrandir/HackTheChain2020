bd6c7ff139fbcf713bbccaec3b4d8d7bed66ffb7
Leyla Shakirzyanova <56456307+leylashakir@users.noreply.github.com>
2020-05-17 02:52:49 -0400

    Uploaded controller file with some starter click methods

---
+++ src/Controller.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+
+public class Controller {
+
+    public void getPrivateKey(javafx.scene.input.MouseEvent mouseEvent) {
+
+    }
+
+    public void enterText(ActionEvent event) {
+        String privateKey;
+        privateKey = TextField.getText();
+    }
+}
